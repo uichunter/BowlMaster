720e4c455ecfc1460f59deef03aae4409d120dae
uichunter <pankettingpanda@gmail.com>
2016-04-15 09:07:14 -0500

    Add a CameraController to let the camera track the ball. And stop in the actual position point.

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject ball;
+	Vector3 subtractVector;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (ball == null) {
+			ball = GameObject.Find("Ball");
+		}
+
+		subtractVector = this.transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (this.transform.position.z <= 1750) {
+			this.transform.position = ball.transform.position + subtractVector;
+		}
+	}
+}
