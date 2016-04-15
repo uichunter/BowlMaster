639f314cfcbdde2fd0c07f728562c6a392afd4ab
uichunter <pankettingpanda@gmail.com>
2016-04-14 21:54:33 -0500

    Create BowlingBall script to control the ball motion.
    Next version I will let the camera track the ball and fallow it.

---
+++ Assets/Script/BowlingBall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BowlingBall : MonoBehaviour {
+	public float ballSpeed;
+	Rigidbody ballRigidbody;
+	AudioSource ballSound;
+	// Use this for initialization
+	void Start ()
+	{
+		ballRigidbody = this.GetComponent<Rigidbody> ();
+		ballSound  = this.GetComponent<AudioSource>();
+
+		BallLaunch ();
+	}
+
+	public void BallLaunch ()
+	{
+		ballRigidbody.velocity = new Vector3 (0f, 0f, ballSpeed);
+		ballSound.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
