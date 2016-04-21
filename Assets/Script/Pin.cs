18c37116d1ed86e18af424533b02c3e6c3d9cb11
uichunter <pankettingpanda@gmail.com>
2016-04-21 17:14:19 -0500

    Create pin.cs and control the pin vector; Create PinSetter and find out the number of standing pins;

---
+++ Assets/Script/Pin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+	public float standingThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool isStanding ()
+	{
+		return Vector3.Angle(transform.up,Vector3.up) < standingThreshold;
+	}
+
+}
---
+++ Assets/Script/PinSetter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PinSetter : MonoBehaviour {
+	Pin[] pinGroup;
+	Text standingText;
+	int standCount = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		pinGroup = FindObjectsOfType<Pin>();
+		standingText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(name +" "+CountStanding());
+	}
+
+	public int CountStanding ()
+	{
+		foreach (Pin pin in pinGroup) {
+			if (pin.isStanding()) {
+				standCount++;
+			}
+		}
+		return standCount;
+	}
+}
