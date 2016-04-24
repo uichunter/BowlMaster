82b77d84e0d368919a2b911c64f4e54be423975a
uichunter <pankettingpanda@gmail.com>
2016-04-24 16:23:50 -0500

    Create some methods to let the pin setter to control the pin rigid body.

--- Assets/Script/Pin.cs
+++ Assets/Script/Pin.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Pin : MonoBehaviour {
 	public float standingThreshold = 5f;
+	Rigidbody pinRigidbody;
 
 	// Use this for initialization
 	void Start () {
+		pinRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +18,16 @@ public class Pin : MonoBehaviour {
 	public bool isStanding ()
 	{
 		return Vector3.Angle(transform.up,Vector3.up) < standingThreshold;
+	}
+
+	public void setUseGravity(bool isUse){
+		pinRigidbody.useGravity = isUse;
+	}
+
+	public void StablePins ()
+	{
+		pinRigidbody.velocity = Vector3.zero;
+		pinRigidbody.angularVelocity = Vector3.zero;
 	}
 
 }
