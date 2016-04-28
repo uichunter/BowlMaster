9d8494119a93e91f9c677544e033567ad96c093b
uichunter <pankettingpanda@gmail.com>
2016-04-28 00:08:22 -0500

    Create a shredder and move some code from the pinsetter.

---
+++ Assets/Script/Shredder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.GetComponentInParent<Pin>()) {
+			Destroy(collider.transform.parent.gameObject);
+		}
+	}
+}
