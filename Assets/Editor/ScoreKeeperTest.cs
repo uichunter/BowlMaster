3c030bc2c4766a722a69c757a642524b17e62887
uichunter <pankettingpanda@gmail.com>
2016-04-30 15:32:20 -0500

    Add a simple test for sore keeping.

---
+++ Assets/Editor/ScoreKeeperTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ScoreKeeperTest {
+
+    [Test]
+    public void EditorTest()
+    {
+		Assert.AreEqual(1,0);
+    }
+}
