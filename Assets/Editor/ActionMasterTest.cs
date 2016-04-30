8ad3797fa6c12375a6eb93ea40c04ab6be28c33c
uichunter <pankettingpanda@gmail.com>
2016-04-30 16:24:06 -0500

    Add a new test for ActionMaster. Create ActionMaster method.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ActionMasterTest {
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+    [Test]
+    public void T01OneStrike ()
+	{
+		ActionMaster actionMaster = new ActionMaster();
+		Assert.AreEqual(endTurn,actionMaster.Bowl(10));
+	}
+}
---
+++ Assets/Script/ActionMaster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+	public enum Action
+	{
+		Tidy, Reset, EndTurn, EndGame
+	}
+
+	public Action Bowl (int pins)
+	{
+		if (pins < 0 || pins > 10) {
+			throw new UnityException("Invaild pins number!");
+		}
+		if (pins == 10) {
+			return Action.EndTurn;
+		}
+
+		throw new UnityException("Dont know which action should return;");
+	}
+}
