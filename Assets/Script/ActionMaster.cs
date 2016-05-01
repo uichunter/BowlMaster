fb9d1750152545f3a4a79758d85ca56fc9e5fad3
uichunter <pankettingpanda@gmail.com>
2016-04-30 23:23:44 -0500

    Finish ActionMaster method.

--- Assets/Script/ActionMaster.cs
+++ Assets/Script/ActionMaster.cs
@@ -2,20 +2,60 @@
 using System.Collections;
 
 public class ActionMaster {
-	public enum Action
-	{
-		Tidy, Reset, EndTurn, EndGame
-	}
+	public enum Action{Tidy, Reset, EndFrame, EndGame}
+
+	private int roll =1;
+
+	private int[] bowls = new int[22];
 
 	public Action Bowl (int pins)
 	{
+		bowls [roll] = pins; 
+
+
+
 		if (pins < 0 || pins > 10) {
-			throw new UnityException("Invaild pins number!");
-		}
-		if (pins == 10) {
-			return Action.EndTurn;
+			throw new UnityException ("Invaild pins number!");
 		}
 
-		throw new UnityException("Dont know which action should return;");
+		//Handle the last frame sitiuation;
+		if (roll >= 21) {// no wonder how many pins, return end game in last frame;
+			return Action.EndGame;
+		}
+
+		if (roll >= 20 && isSpareInLastFrame ()) {// Detect the roll 20;
+			roll += 1;
+			return Action.Reset;
+		} else if (roll == 20 && !isSpareInLastFrame ()) {
+			if (bowls[roll-1] == 10){
+				roll +=1;
+				return Action.Tidy;
+			}
+			return Action.EndGame;
+		}
+
+		//If player hit a strike;
+		if (pins == 10) {
+			if (roll >= 19) {// Handle last frame;
+				roll +=1;
+				return Action.Reset;
+			}
+			roll +=2;
+			return Action.EndFrame;
+		}
+
+		//General sitiuation;
+		if (roll % 2 == 1) {
+			roll +=1;
+			return Action.Tidy;
+		} else {
+			roll +=1;
+			return Action.EndFrame;
+		}
+	}
+
+	private bool isSpareInLastFrame ()
+	{
+		return (bowls[roll-1]+bowls[roll] ==10 && bowls[roll -1] !=10);
 	}
 }
