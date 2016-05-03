1fd84f286d39e47bc01a612a368bc2552904eaab
uichunter <pankettingpanda@gmail.com>
2016-05-02 23:26:55 -0500

    Create Game Manager.

---
+++ Assets/Script/Refactor/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+	
+	public Text standingText;
+
+	ActionMaster actionMaster; 
+
+	
+	// Use this for initialization
+	void Start () {
+		actionMaster = gameObject.GetComponent<ActionMaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		standingText.text = CountStanding ().ToString ();
+		if (isBallEnterBox) {
+			CheckPinSettle();
+		}
+	}
+
+	void SendPinFall (int pinFall)
+	{
+		actionMaster.Bowl(pinFall);
+	}
+}
+
+//class Frames : IList{
+//	
+//}
